6e237fde624c554233fd6d755561faf0e5f1109e
Semdelil <75840973+Semdelil@users.noreply.github.com>
2022-06-20 10:16:34 +0200

    ok

---
+++ pages/start.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>30 SECS</title>
+	<link rel="stylesheet" type="text/css" href="../css/start.css">
+	<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+</head>
+<body>
+
+<header>
+	<div class="header_menu">
+		<div class="logo"> 
+			<img src="bg1.png" alt="."> 
+		</div>
+		<div class="menu_list">
+			<ul>
+				<li><a href="home.php" >test</a></li>
+				<li><a href="game.php">Game</a> </li>
+		
+				
+				
+				
+			</ul>
+		</div>
+	</div>
+
+	<div class="header_content">
+		<h2>Onze 30 Seconds game</h2>
+		<h1>Veel speel plezier</h1>
+		<button><a href="#">
+			Click
+		</a></button>
+
+		<button><a href="#">
+			...
+		</a></button>
+	</div>
+</header>
