5dc26558bee748345c0dc94ef0b7ca9c8884bb64
Semdelil <75840973+Semdelil@users.noreply.github.com>
2022-06-20 11:03:03 +0200

    ok

---
+++ pages/end.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>30 SECS</title>
+    <link rel="stylesheet" type="text/css" href="../css/start.css">
+    <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+</head>
+<body>
+
+<header>
+    <div class="header_menu">
+        <div class="logo"> 
+            <img src="../images/bg2.png" alt="."> 
+        </div>
+        <div class="menu_list">
+            <ul>
+
+
+
+
+
+            </ul>
+        </div>
+    </div>
+
+    <div class="header_content">
+        <h2>Helaas</h2>
+        <h1>Einde spel</h1>
+        <button><a href="#">
+            restart
+        </a></button>
+
+        <button><a href="#">
+
+😢
+        </a></button>
+    </div>
+</header>
